3d80e5b1acae403f7991e12f19480080a1369270
juanpguerreroleal <juanpabloguleal@gmail.com>
2025-02-05 00:14:23 -0600

    Fix to service card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -31,7 +31,7 @@ export const ServiceCard = () => {
           data-aos="fade-left"
         >
           <img
-            src={service.attributes.Img}
+            src={ service.attributes.Img ? service.attributes.Img.data.attributes.url : '/UI/src/assets/logonav.svg' }
             className="h-full w-full object-cover rounded-2xl opacity-60"
             alt={service.attributes.Name}
             loading="lazy"
