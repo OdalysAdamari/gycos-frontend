a5bf4d84520dcac960ef9798e0c0726e98ba3f92
juanpguerreroleal <juanpabloguleal@gmail.com>
2025-02-09 00:17:34 -0600

    Fixing portafolio images display and updating aboutus images

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -5,7 +5,7 @@ export const AboutUs = () => {
         <div className="font-inter bg-gray-50 text-gray-800">
       
           
-          <header className="relative py-16 px-10 bg-fixed bg-cover bg-center text-center text-white  flex flex-col items-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1593106421907-821f022cf8c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')" }}>
+          <header className="relative py-16 px-10 bg-fixed bg-cover bg-center text-center text-white  flex flex-col items-center" style={{ backgroundImage: "url('/uploads/aboutus_background_19d448784e.png')" }}>
             <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
             <div className="relative z-10">
             <h1 className="text-4xl font-bold">GYCOS S.A. DE C.V.</h1>
@@ -24,12 +24,12 @@ export const AboutUs = () => {
               </p>
             </div>
             <div className="md:w-1/2 flex justify-center">
-              <img className="rounded-lg shadow-lg" src="https://images.unsplash.com/photo-1593106421907-821f022cf8c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Company Image" />
+              <img className="rounded-lg shadow-lg" src="/uploads/aboutus_cover_07723d5cbd.png" alt="Company Image" />
             </div>
           </section>
       
          
-          <section className="relative py-16 px-10 bg-fixed bg-cover bg-center text-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1593106421907-821f022cf8c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')" }}>
+          <section className="relative py-16 px-10 bg-fixed bg-cover bg-center text-center" style={{ backgroundImage: "url('/uploads/aboutus_background_19d448784e.png')" }}>
             <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
             <div className="relative z-10">
               <h2 className="text-3xl font-bold text-white mb-8">POR QUÉ ELEGIRNOS</h2>
@@ -47,28 +47,6 @@ export const AboutUs = () => {
             </div>
           </section>
       
-        
-          <section className="py-16 px-10 bg-white">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-              <div className="flex justify-center">
-                <img className="rounded-lg shadow-lg" src="https://images.unsplash.com/photo-1593106421907-821f022cf8c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Partner Image" />
-              </div>
-              <div className="flex flex-col justify-center text-center md:text-left">
-                <h2 className="text-2xl md:text-4xl font-bold mb-4">SOCIOS COMERCIALES</h2>
-                <p className="text-lg md:text-2xl mb-4">
-                  GYCOS S.A. DE C.V. tiene una empresa hermana llamada
-                </p>
-                <p className="text-lg md:text-2xl font-bold mb-4">
-                  GP GRUPO INDUSTRIAL Y ADMINISTRATIVO SA DE CV
-                </p>
-                <p className="text-lg md:text-2xl">
-                  dedicada al rubro administrativo e industrial.
-                </p>
-              </div>
-            </div>
-          </section>
-      
-         
           <section className="bg-gray-800 text-gray-100 py-16 text-center">
             <h2 className="text-3xl font-bold">Contáctanos</h2>
             <span className="text-lg md:text-2xl block mt-4">Formulario de Contacto</span>
